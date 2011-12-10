c9fbd2ee24b861d72ded48858a78ca6692e47031
Herwig Hochleitner <hhochleitner@gmail.com>
2011-12-10 02:07:38 +0100

    Put game starter kit into a leiningen project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject aichallenge "0.1.0-SNAPSHOT"
+  :description "Clojure implementation of google's aichallenge"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.logic "0.6.7"]]
+  :dev-dependencies [[clj-http "0.2.5"]])
