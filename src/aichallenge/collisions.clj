1d101579aaa71cb5f4c7188dc258a9520d1510c9
Robert Campbell <rrc@robert-campbell.com>
2011-12-11 02:50:24 +0100

    hide non-api fns

--- src/aichallenge/collisions.clj
+++ src/aichallenge/collisions.clj
@@ -2,7 +2,7 @@
   (:use [clojure.set :only (difference)])
   (:require [aichallenge.ant :as ant]))
 
-(defn collisions [moves]
+(defn- collisions [moves]
   (reduce (fn [accum move]
             (let [next-loc (apply ant/move-ant move)]
               (if (accum next-loc)
@@ -11,7 +11,7 @@
           {}
           moves))
 
-(defn find-alternative [m ant dirs]
+(defn- find-alternative [m ant dirs]
   (let [poss (map (fn [ant dir]
                     [ant dir (ant/move-ant ant dir)])
                   (repeat ant)
@@ -19,7 +19,7 @@
     (first (remove (fn [[_ _ next-move]] (m next-move))
                        poss))))
 
-(defn all-alternatives [collisions]
+(defn- all-alternatives [collisions]
   (for [collision collisions
         :let  [moves (val collision)]
         :when (> (count moves) 1)
@@ -27,7 +27,7 @@
     (find-alternative collisions ant (difference directions #{dir}))))
         
 (let [directions #{:north :south :east :west}]
-  (defn all-alternatives [collisions]
+  (defn- all-alternatives [collisions]
     (for [collision collisions
           :let  [moves (val collision)]
           :when (> (count moves) 1)
