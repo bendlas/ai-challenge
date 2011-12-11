09cb3fc2df56eef4a73d44606232c3952f6f6e8e
Georgy Bolyuba <bolyuba@gmail.com>
2011-12-11 19:38:49 +0100

    Added demo.sh for the full game

---
+++ demo.sh
@@ -0,0 +1,13 @@
+tools/playgame.py \
+--player_seed 42 \
+--end_wait=0.25 \
+--verbose \
+--log_dir tools/game_logs \
+--turns 1000 \
+--map_file tools/maps/maze/maze_04p_01.map \
+-e \
+--strict \
+--capture_errors \
+--loadtime=10000 \
+"$@" "python tools/sample_bots/python/HunterBot.py" "python tools/sample_bots/python/LeftyBot.py" "python tools/sample_bots/python/GreedyBot.py" "java -cp `lein classpath` clojure.main @MyBot.clj" \
+-So | java -jar tools/visualizer.jar
